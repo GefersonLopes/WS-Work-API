2392e933291b9dfbf27c3cdb864134fbf58cc90c
Geferson Lopes <gefersonjefreey@gmail.com>
2025-08-14 16:28:54 -0300

    chore: initialize project architecture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BrandsModule } from './modules/brands/brands.module';
+import { ModelsModule } from './modules/models/models.module';
+import { CarsModule } from './modules/cars/cars.module';
 
 @Module({
-  imports: [],
+  imports: [BrandsModule, ModelsModule, CarsModule],
   controllers: [AppController],
   providers: [AppService],
 })
